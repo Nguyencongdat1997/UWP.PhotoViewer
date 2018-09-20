0b48206da011e2770a24743aece1a514bca44609
DatGatto <traceformula2000@gmail.com>
2018-09-20 14:58:16 +0700

    PhotoViewer | First Implementation

---
+++ PhotoViewer/Data/IPhoto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoViewer.Data
+{
+    public interface IPhoto
+    {
+        String Uri { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
